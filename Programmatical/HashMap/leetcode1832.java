d6f2353762a2f16a75909049e6554a39a423f750
ravindra0602 <ravindrareddyk5248@gmail.com>
2023-04-25 22:25:03 +0530

    class Solution {

---
+++ Programmatical/HashMap/leetcode1832.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        // we can also this problem by using HashSet in java.
+        HashMap<Character,Integer> Freq = new HashMap<>();
+        for(int i = 0; i < sentence.length(); i++)
+        {
+            if(Freq.containsKey(sentence.charAt(i))) Freq.put(sentence.charAt(i), Freq.get(sentence.charAt(i) + 1));
+            else Freq.put(sentence.charAt(i),1);
+        }
+        return Freq.size() == 26;
+    }
+}
