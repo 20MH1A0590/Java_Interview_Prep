d2d679fb03c934273361d56a680764324303a035
ravindra0602 <ravindrareddyk5248@gmail.com>
2023-04-30 12:36:34 +0530

    Reverse Words in a String III

---
+++ Programmatical/Strings/leetcode557.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        String words [] = s.split(" ");
+        String ans = "";
+        for(int i = 0; i < words.length; i++)
+        {
+            // string class is immutable in java.
+            // here we are convert each string(word) in the given sentence to the StringBuffer class.
+            StringBuffer sb = new StringBuffer(words[i]);
+            if(i != words.length-1) ans += sb.reverse() + " ";
+            else ans += sb.reverse();
+        }
+        return ans;     
+    }
+}
